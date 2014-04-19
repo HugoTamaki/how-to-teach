e73c2675cdc976888f74d35367508a33f196c66f
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-04-19 20:14:09 -0300

    validando para usuário não se adicionar

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,12 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+
+  validate :cant_add_yourself
+
+  def cant_add_yourself
+    if user_id == friend_id
+      errors.add(:friend_id, "Você não pode adicionar a si mesmo")
+    end
+  end
 end
