3909101bf6d82eebeab4b97ead3fa7f95b70816c
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-04-19 20:14:37 -0300

    mudando layout de perfil, perfil dos outros e menu

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,15 +6,13 @@ class FriendshipsController < ApplicationController
       redirect_to my_profile_path, notice: "Amigo adicionado com sucesso!"
     else
       redirect_to profile_path(params[:friend_id]), alert: "Não foi possivel adicionar o amigo."
-      binding.pry
-      # flash[:error] = "Não foi possivel adicionar o amigo"
     end
   end
 
   def destroy
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
-    flash[:notice] = "Removed friendship."
-    redirect_to current_user
+    flash[:notice] = "Amigo removido."
+    redirect_to my_profile_path
   end
 end
