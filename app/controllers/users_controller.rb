7db4136556aee89f4ce56308e868ed7403eb54f9
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-04-20 22:14:08 -0300

    incluindo autenticação para controlador de users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  
   def my_profile
     @user = User.find(current_user.id)
     redirect_to search_path if params[:search]
