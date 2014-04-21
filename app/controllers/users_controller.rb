e433a2dfd73931a26f7458dbddfdee406f451dab
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-04-21 16:57:56 -0300

    melhorando funcionalidade de busca

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,35 @@ class UsersController < ApplicationController
   
   def my_profile
     @user = User.find(current_user.id)
-    redirect_to search_path if params[:search]
+    @search = User.search(params[:search]) if params[:search]
+    render "search" if params[:search]
   end
 
   def profile
     @user = User.find(params[:id])
     @search = User.search(params[:search]) if params[:search]
-    render my_profile_path if params[:search]
+    render "search" if params[:search]
   end
 
   def search
-    @search = User.search(params[:search]) if params[:search]
+    
+  end
+
+  def accept
+    @friendship = Friendship.find(params[:id])
+    @friend_friendship = Friendship.where(user_id: params[:friend_id]).first
+    @friendship.accepted = true
+    @friend_friendship.accepted = true
+    if @friendship.save && @friend_friendship.save
+        redirect_to my_profile_path, notice: "Amigo aceito com sucesso."
+      else
+        redirect_to my_profile_path, alert: "Não foi possível aceitar o amigo."
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.search(search)
-    # find(:all, :conditions => ['first_name LIKE ? or last_name LIKE ? or users.methodologies.title = ?', "%#{search}%", "%#{search}%", "%#{search}%"]) if search
-    # joins(:methodologies).where("users.first_name LIKE ? or users.last_name LIKE ? or methodologies.title LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") if search
     Methodology.joins(:user).where("users.first_name LIKE ? or users.last_name LIKE ? or methodologies.title LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") if search
   end
 
