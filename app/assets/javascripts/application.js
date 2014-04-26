27c7745e18a761c13ffec04b61a1a90b04a069b1
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-04-26 20:06:32 -0300

    acrescentando light box para videos de metodologia

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,26 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+    console.log("ahh");
+    $(".fancybox").click(function() {
+      $.fancybox({
+          'padding'   : 0,
+          'autoScale'   : false,
+          'transitionIn'  : 'none',
+          'transitionOut' : 'none',
+          'title'     : this.title,
+          'width'   : 680,
+          'height'    : 495,
+          'href'      : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+          'type'      : 'swf',
+          'swf'     : {
+               'wmode'    : 'transparent',
+            'allowfullscreen' : 'true'
+          }
+        });
+
+      return false;
+    });
+});
