757562737b5f11902333e104ff6bb50f883eb696
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-04-27 14:05:09 -0300

    refatorando esquema de busca

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,22 @@ class UsersController < ApplicationController
   
   def my_profile
     @user = User.find(current_user.id)
-    @search = User.search(params[:search]) if params[:search]
-    render "search" if params[:search]
+    search_path params[:search] if params[:search]
   end
 
   def profile
     @user = User.find(params[:id])
-    @search = User.search(params[:search]) if params[:search]
-    render "search" if params[:search]
-  end
-
-  def search
-    
+    search_path params[:search] if params[:search]
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name)
-  end
+    def search_path(search)
+      @search = User.search(search)
+      render "search"
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name)
+    end
 end
