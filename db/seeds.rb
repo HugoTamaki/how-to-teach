1848dd0ab3aad6a23f14c2aa408fc727fa34c942
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-05-21 21:14:31 -0300

    terminando de criar sede de usuários

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,37 @@
 # preenchendo usuários
 users = [
   {
-    first_name: "Rafael",
-    last_name: "Andrade",
-    email: "rafael@exemplo.com",
+    first_name: "Hugo",
+    last_name: "Tamaki",
+    email: "hugotamaki@exemplo.com",
+    password: "11111111",
+    password_confirmation: "11111111"
+  },
+  {
+    first_name: "Jéssika",
+    last_name: "Guedes",
+    email: "jessika@exemplo.com",
     password: "11111111",
     password_confirmation: "11111111"
   },
   {
     first_name: "Maria",
     last_name: "Isabel",
-    email: "maria@exemplo.com",
+    email: "maria_isabel@exemplo.com",
+    password: "11111111",
+    password_confirmation: "11111111"
+  },
+  {
+    first_name: "Roberta",
+    last_name: "Fernandes",
+    email: "roberta@exemplo.com",
+    password: "11111111",
+    password_confirmation: "11111111"
+  },
+  {
+    first_name: "Larissa",
+    last_name: "Fernandes",
+    email: "larissa@exemplo.com",
     password: "11111111",
     password_confirmation: "11111111"
   }
@@ -44,46 +65,91 @@ schools << [
   }
 ]
 
+schools << [
+  {
+    name: "C.E. Pedro II"
+  },
+  {
+    name: "C.E. Aurelino Leal"
+  }
+]
+
+schools << [
+  {
+    name: "C.E. Joaquim Tavora"
+  },
+  {
+    name: "C.E. Manoel de Abreu"
+  }
+]
+
+schools << [
+  {
+    name: "C.E. Maria Joaquina"
+  },
+  {
+    name: "C.E. Lerolero"
+  }
+]
+
 # preenchendo metodologias
 methodologies = []
 methodologies << [
   {
-    title: "Como aprender Java",
-    content: "Esta metodologia visa ensinar Java para iniciantes.\n
-              Java é uma linguagem muito importante e usada em vários sistemas hoje em dia, \n
-              e foi uma revolução para a programação, inserindo o paradigma da Orientação a Objetos \n
-              para o desenvolvimento de sistemas.
-              ...
-             "
-  },
-  {
-    title: "Aprendendo Pascal",
-    content: "Pascal não é uma linguagem difícil. Geralmente a primeira coisa a se ensinar é: \n
-              O conceito de variáveis\n
-              O conceito de blocos condicionais\n
-              O conceito de blocos de repetição\n
-              O conceito de vetores.
-             "
+    title: "Como aprender Ruby",
+    teaser: "Aprenda ruby de forma rápida e eficiente aqui!",
+    content: "Esta metodologia visa ensinar Ruby para iniciantes.\n
+Ruby é uma linguagem orientada a objetos, criada no Japão na década de 90, e só foi ser conhecido 
+no ocidente em 2004 graças ao sucesso do framework Ruby on Rails que serve para fazer 
+aplicações web de forma ágil e eficiente. \nÉ uma linguagem que chama atenção pela simplicidade 
+e pela proximidade com a linguagem humana...
+"
   }
 ]
 
 methodologies << [
   {
-    title: "Ensinando Ruby",
-    content: "Ruby é a nova moda do mundo da programação web. Muitos empregos surgiram \n
-              a partir de 2004 (época em que o principal framework fez sucesso, o Ruby on Rails), \n
-              e hoje, após 10 anos de evolucão, muitas empresas grandes já usam Ruby. 
-             "
-  },
-  {
-    title: "Ensinando Ruby",
-    content: "Ruby é a nova moda do mundo da programação web. Muitos empregos surgiram \n
-              a partir de 2004 (época em que o principal framework fez sucesso, o Ruby on Rails), \n
-              e hoje, após 10 anos de evolucão, muitas empresas grandes já usam Ruby. 
-             "
+    title: "Aprendendo Pascal",
+    teaser: "Aprenda a linguagem que ensina de modo fácil a programar!",
+    content: "Pascal não é uma linguagem difícil. Geralmente a primeira coisa a se ensinar é: \n
+O conceito de variáveis\n
+O conceito de blocos condicionais\n
+O conceito de blocos de repetição\n
+O conceito de vetores.
+"
   }
 ]
 
+methodologies << [
+  {
+    title: "vetores em Javascript de forma fácil",
+    teaser: "Aprenda como fazer vetores em Javascript de forma fácil!",
+    content: "Javascript é uma linguagem dinâmica e muito eficiente para o lado do cliente na 
+web. \nVocê pode aprender como fazer vetores lerolero"
+  }
+]
+
+methodologies << [
+  {
+    title: "Como fazer um App para o Firefox OS",
+    teaser: "Um tutorial muito rápido de como fazer um app!",
+    content: "Aprenda a fazer um aplicativo para firefox os. Para isso você deve conhecer 
+html e js de forma eficiente. Se você já tem um bom conhecimento de javascript, pode começar 
+baixando o kit de desenvolvimento da mozilla..."
+  }
+]
+
+methodologies << [
+  {
+    title: "Aprendendo Pascal",
+    content: "Pascal não é uma linguagem difícil. Geralmente a primeira coisa a se ensinar é: \n
+O conceito de variáveis\n
+O conceito de blocos condicionais\n
+O conceito de blocos de repetição\n
+O conceito de vetores.
+"
+  }
+]
 
 users.each_with_index do |user, i|
   User.create!(user)
