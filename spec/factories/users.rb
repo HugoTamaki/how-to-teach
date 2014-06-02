c652fc3ad927f63296f759b49ac0c4f5c2b09854
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-06-02 07:48:47 -0300

    adicionando nome em factory de user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
+    first_name "Hugo"
+    last_name "Tamaki"
     email "exemplo@exemplo.com"
     password "11111111"
     password_confirmation "11111111"
